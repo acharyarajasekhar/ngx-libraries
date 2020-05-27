498cd1e76bf9816c4b57e7a266af4f92e80ca756
acharyarajasekhar <acharya.r@live.com>
2020-05-27 22:45:49 +0530

    ngx-utility-pipes plugin

---
+++ projects/ngx-utility-pipes/src/lib/ngx-utility-pipes.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { TimeAgoPipe } from './pipes/time-ago.pipe';
+
+@NgModule({
+  declarations: [TimeAgoPipe],
+  imports: [
+  ],
+  exports: [TimeAgoPipe]
+})
+export class NgxUtilityPipesModule { }
---
+++ projects/ngx-utility-pipes/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ngx-utility-pipes
+ */
+
+export * from './lib/pipes/time-ago.pipe';
+export * from './lib/ngx-utility-pipes.module';
